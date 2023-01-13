8b9711185c17df2af6ed39eb9c2a1b020f8986fc
wendet <wendeal@gmail.com>
2023-01-13 14:33:20 +0000

    CRF integrated by commenting the model call

---
+++ src/toxic_x_dom/span_prediction/crf_models.py
@@ -0,0 +1,155 @@
+from transformers import BertPreTrainedModel, BertModel
+from torch import nn
+import torch
+from torchcrf import CRF
+
+
+# torch.cuda.set_device(0)
+
+
+class BertCRF(BertPreTrainedModel):
+    _keys_to_ignore_on_load_unexpected = [r"pooler"]
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = BertModel(config, add_pooling_layer=False)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.crf = CRF(num_tags=config.num_labels, batch_first=True)
+        self.init_weights()
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            labels=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the token classification loss. Indices should be in ``[0, ..., config.num_labels -
+            1]``.
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            # print('labels.shape ', labels.shape)
+            # print('logits.shape ', logits.shape)
+            # print('attention_mask.shape ', attention_mask.shape)
+            # print('labels ', labels)
+            # # print('logits ', logits)
+            # print('type(attention_mask) ', type(attention_mask))
+            # print('attention_mask ', attention_mask)
+            # log_likelihood = self.crf(logits, labels, attention_mask)
+
+            attention_mask = attention_mask.type(torch.bool)  # Cast into boolean tensor
+            labels[labels == -100] = 0  # Fix for "IndexError: index -100 is out of bounds for dimension 0 with size 3"
+
+            log_likelihood = self.crf(logits, labels, attention_mask)
+            tags = self.crf.decode(logits)
+            loss = 0 - log_likelihood
+        else:
+            tags = self.crf.decode(logits)
+        tags = torch.Tensor(tags)
+
+        if not return_dict:
+            output = (tags,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return loss, tags
+
+
+class BertLstmCRF(BertPreTrainedModel):
+
+    _keys_to_ignore_on_load_unexpected = [r"pooler"]
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = BertModel(config, add_pooling_layer=False)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        # self.bilstm = nn.LSTM(config.hidden_size, (config.hidden_size) // 2, dropout=config.dropout, batch_first=True,
+        #                       bidirectional=True)
+        self.bilstm = nn.LSTM(config.hidden_size, (config.hidden_size) // 2, batch_first=True,
+                              bidirectional=True)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.crf = CRF(num_tags=config.num_labels, batch_first=True)
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the token classification loss. Indices should be in ``[0, ..., config.num_labels -
+            1]``.
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+        sequence_output = self.dropout(sequence_output)
+        lstm_output, hc = self.bilstm(sequence_output)
+        logits = self.classifier(lstm_output)
+
+        loss = None
+        if labels is not None:
+            labels[labels == -100] = 0  # Fix for "IndexError: index -100 is out of bounds for dimension 0 with size 3"
+            log_likelihood, tags = self.crf(logits, labels), self.crf.decode(logits)
+            loss = 0 - log_likelihood
+        else:
+            tags = self.crf.decode(logits)
+        tags = torch.Tensor(tags)
+
+        if not return_dict:
+            output = (tags,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return loss, tags
