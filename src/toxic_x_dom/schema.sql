684efe5bc5a6937febc318823efa9668ddcc9095
Stefan Schouten <sfschouten@gmail.com>
2023-02-23 15:10:02 +0100

    Update schema.sql

--- src/toxic_x_dom/schema.sql
+++ src/toxic_x_dom/schema.sql
@@ -150,108 +150,115 @@ LEFT OUTER JOIN rationale_evaluation AS re ON e_max_p.id = re.id;
 
 CREATE VIEW tuned_in_domain_max_f1_har_macro_view AS
 	WITH evaluation_plus AS (
-		SELECT *,
-		CASE
-			WHEN le.id IS NOT NULL THEN 'lexicon'
-			WHEN pe.id IS NOT NULL THEN 'span_pred'
-			WHEN re.id IS NOT NULL THEN 'rationale'
-			ELSE ''
-		END AS method_type
-		FROM evaluation AS e
-		LEFT OUTER JOIN lexicon_evaluation AS le ON e.id = le.id
-		LEFT OUTER JOIN span_pred_evaluation AS pe ON e.id = pe.id
-		LEFT OUTER JOIN rationale_evaluation AS re ON e.id = re.id
+	    SELECT *,
+	    CASE
+	        WHEN le.id IS NOT NULL THEN 'lexicon'
+	        WHEN pe.id IS NOT NULL THEN 'span_pred'
+	        WHEN re.id IS NOT NULL THEN 'rationale'
+	        ELSE ''
+	    END AS method_type
+	    FROM evaluation AS e
+	    LEFT OUTER JOIN lexicon_evaluation AS le ON e.id = le.id
+	    LEFT OUTER JOIN span_pred_evaluation AS pe ON e.id = pe.id
+	    LEFT OUTER JOIN rationale_evaluation AS re ON e.id = re.id
 	), in_domain_max AS (
-		SELECT method_type, train_dataset, eval_dataset, MAX(f1_har_macro) AS max_f1_har_macro
-		FROM evaluation_plus
-		WHERE train_dataset = eval_dataset
-		GROUP BY method_type, train_dataset, eval_dataset
+	    SELECT method_type, train_dataset, lexicon_key, attribution_method, propagate_binary, eval_dataset, MAX(f1_har_macro) AS max_f1_har_macro
+	    FROM evaluation_plus
+	    WHERE train_dataset = eval_dataset
+	    GROUP BY method_type, train_dataset, lexicon_key, attribution_method, propagate_binary, eval_dataset
 	), in_domain_max_ids AS (
-		SELECT evaluation_plus.id
-		FROM in_domain_max
-		LEFT OUTER JOIN evaluation_plus
-		ON in_domain_max.method_type = evaluation_plus.method_type
-		AND in_domain_max.train_dataset = evaluation_plus.train_dataset
-		AND in_domain_max.eval_dataset = evaluation_plus.eval_dataset
-		AND in_domain_max.max_f1_har_macro = evaluation_plus.f1_har_macro
+	    SELECT evaluation_plus.id
+	    FROM in_domain_max
+	    LEFT OUTER JOIN evaluation_plus
+	    ON in_domain_max.method_type = evaluation_plus.method_type
+	    AND in_domain_max.train_dataset = evaluation_plus.train_dataset
+	    AND in_domain_max.lexicon_key IS NOT DISTINCT FROM evaluation_plus.lexicon_key
+	    AND in_domain_max.attribution_method IS NOT DISTINCT FROM evaluation_plus.attribution_method
+	    AND in_domain_max.propagate_binary = evaluation_plus.propagate_binary
+	    AND in_domain_max.eval_dataset = evaluation_plus.eval_dataset
+	    AND in_domain_max.max_f1_har_macro = evaluation_plus.f1_har_macro
 	), cross_domain_max AS (
-		SELECT e_cross.*
-		FROM in_domain_max_ids AS e
-		LEFT OUTER JOIN evaluation_plus AS e_in
-		ON e.id = e_in.id
-		LEFT OUTER JOIN evaluation_plus AS e_cross
-		ON  e_in.propagate_binary IS NOT DISTINCT FROM e_cross.propagate_binary
-		AND e_in.filling_chars IS NOT DISTINCT FROM e_cross.filling_chars
-		AND e_in.train_dataset = e_cross.train_dataset
-		AND (
-			(
-				e_in.method_type = 'lexicon' AND e_in.method_type = e_cross.method_type
-				AND e_in.theta IS NOT DISTINCT FROM e_cross.theta
-				AND e_in.min_occurrence IS NOT DISTINCT FROM e_cross.min_occurrence
-			) OR (
-				e_in.method_type = 'rationale' AND e_in.method_type = e_cross.method_type
-				AND e_in.attribution_method IS NOT DISTINCT FROM e_cross.attribution_method
-				AND e_in.scale_scores IS NOT DISTINCT FROM e_cross.scale_scores
-				AND e_in.cumulative_scoring IS NOT DISTINCT FROM e_cross.cumulative_scoring
-				AND e_in.threshold IS NOT DISTINCT FROM e_cross.threshold
-			) OR (
-				e_in.method_type = 'span_pred' AND e_in.method_type = e_cross.method_type
-			)
-		)
+	    SELECT e_cross.*
+	    FROM in_domain_max_ids AS e
+	    LEFT OUTER JOIN evaluation_plus AS e_in
+	    ON e.id = e_in.id
+	    LEFT OUTER JOIN evaluation_plus AS e_cross
+	    ON  e_in.propagate_binary IS NOT DISTINCT FROM e_cross.propagate_binary
+	    AND e_in.filling_chars IS NOT DISTINCT FROM e_cross.filling_chars
+	    AND e_in.train_dataset = e_cross.train_dataset
+	    AND (
+	        (
+	            e_in.method_type = 'lexicon' AND e_in.method_type = e_cross.method_type
+	            AND e_in.lexicon_key IS NOT DISTINCT FROM e_cross.lexicon_key
+	            AND e_in.theta IS NOT DISTINCT FROM e_cross.theta
+	            AND e_in.min_occurrence IS NOT DISTINCT FROM e_cross.min_occurrence
+	        ) OR (
+	            e_in.method_type = 'rationale' AND e_in.method_type = e_cross.method_type
+	            AND e_in.attribution_method IS NOT DISTINCT FROM e_cross.attribution_method
+	            AND e_in.scale_scores IS NOT DISTINCT FROM e_cross.scale_scores
+	            AND e_in.cumulative_scoring IS NOT DISTINCT FROM e_cross.cumulative_scoring
+	            AND e_in.threshold IS NOT DISTINCT FROM e_cross.threshold
+	        ) OR (
+	            e_in.method_type = 'span_pred' AND e_in.method_type = e_cross.method_type
+	        )
+	    )
 	)
 	SELECT * FROM cross_domain_max;
 
 
 CREATE VIEW tuned_in_domain_max_f1_toxic_view AS
 	WITH evaluation_plus AS (
-		SELECT *,
-		CASE
-			WHEN le.id IS NOT NULL THEN 'lexicon'
-			WHEN pe.id IS NOT NULL THEN 'span_pred'
-			WHEN re.id IS NOT NULL THEN 'rationale'
-			ELSE ''
-		END AS method_type
-		FROM evaluation AS e
-		LEFT OUTER JOIN lexicon_evaluation AS le ON e.id = le.id
-		LEFT OUTER JOIN span_pred_evaluation AS pe ON e.id = pe.id
-		LEFT OUTER JOIN rationale_evaluation AS re ON e.id = re.id
+	    SELECT *,
+	    CASE
+	        WHEN le.id IS NOT NULL THEN 'lexicon'
+	        WHEN pe.id IS NOT NULL THEN 'span_pred'
+	        WHEN re.id IS NOT NULL THEN 'rationale'
+	        ELSE ''
+	    END AS method_type
+	    FROM evaluation AS e
+	    LEFT OUTER JOIN lexicon_evaluation AS le ON e.id = le.id
+	    LEFT OUTER JOIN span_pred_evaluation AS pe ON e.id = pe.id
+	    LEFT OUTER JOIN rationale_evaluation AS re ON e.id = re.id
 	), in_domain_max AS (
-		SELECT method_type, train_dataset, eval_dataset, MAX(f1_toxic) AS max_f1_toxic
-		FROM evaluation_plus
-		WHERE train_dataset = eval_dataset
-		GROUP BY method_type, train_dataset, eval_dataset
+	    SELECT method_type, train_dataset, lexicon_key, attribution_method, propagate_binary, eval_dataset, MAX(f1_toxic) AS max_f1_toxic
+	    FROM evaluation_plus
+	    WHERE train_dataset = eval_dataset
+	    GROUP BY method_type, train_dataset, lexicon_key, attribution_method, propagate_binary, eval_dataset
 	), in_domain_max_ids AS (
-		SELECT evaluation_plus.id
-		FROM in_domain_max
-		LEFT OUTER JOIN evaluation_plus
-		ON in_domain_max.method_type = evaluation_plus.method_type
-		AND in_domain_max.train_dataset = evaluation_plus.train_dataset
-		AND in_domain_max.eval_dataset = evaluation_plus.eval_dataset
-		AND in_domain_max.max_f1_toxic = evaluation_plus.f1_toxic
+	    SELECT evaluation_plus.id
+	    FROM in_domain_max
+	    LEFT OUTER JOIN evaluation_plus
+	    ON in_domain_max.method_type = evaluation_plus.method_type
+	    AND in_domain_max.train_dataset = evaluation_plus.train_dataset
+	    AND in_domain_max.lexicon_key IS NOT DISTINCT FROM evaluation_plus.lexicon_key
+	    AND in_domain_max.attribution_method IS NOT DISTINCT FROM evaluation_plus.attribution_method
+	    AND in_domain_max.propagate_binary = evaluation_plus.propagate_binary
+	    AND in_domain_max.eval_dataset = evaluation_plus.eval_dataset
+	    AND in_domain_max.max_f1_toxic = evaluation_plus.f1_toxic
 	), cross_domain_max AS (
-		SELECT e_cross.*
-		FROM in_domain_max_ids AS e
-		LEFT OUTER JOIN evaluation_plus AS e_in
-		ON e.id = e_in.id
-		LEFT OUTER JOIN evaluation_plus AS e_cross
-		ON  e_in.propagate_binary IS NOT DISTINCT FROM e_cross.propagate_binary
-		AND e_in.filling_chars IS NOT DISTINCT FROM e_cross.filling_chars
-		AND e_in.train_dataset = e_cross.train_dataset
-		AND (
-			(
-				e_in.method_type = 'lexicon' AND e_in.method_type = e_cross.method_type
-				AND e_in.theta IS NOT DISTINCT FROM e_cross.theta
-				AND e_in.min_occurrence IS NOT DISTINCT FROM e_cross.min_occurrence
-			) OR (
-				e_in.method_type = 'rationale' AND e_in.method_type = e_cross.method_type
-				AND e_in.attribution_method IS NOT DISTINCT FROM e_cross.attribution_method
-				AND e_in.scale_scores IS NOT DISTINCT FROM e_cross.scale_scores
-				AND e_in.cumulative_scoring IS NOT DISTINCT FROM e_cross.cumulative_scoring
-				AND e_in.threshold IS NOT DISTINCT FROM e_cross.threshold
-			) OR (
-				e_in.method_type = 'span_pred' AND e_in.method_type = e_cross.method_type
-			)
-		)
+	    SELECT e_cross.*
+	    FROM in_domain_max_ids AS e
+	    LEFT OUTER JOIN evaluation_plus AS e_in
+	    ON e.id = e_in.id
+	    LEFT OUTER JOIN evaluation_plus AS e_cross
+	    ON  e_in.propagate_binary IS NOT DISTINCT FROM e_cross.propagate_binary
+	    AND e_in.filling_chars IS NOT DISTINCT FROM e_cross.filling_chars
+	    AND e_in.train_dataset = e_cross.train_dataset
+	    AND (
+	        (
+	            e_in.method_type = 'lexicon' AND e_in.method_type = e_cross.method_type
+	            AND e_in.lexicon_key IS NOT DISTINCT FROM e_cross.lexicon_key
+	            AND e_in.theta IS NOT DISTINCT FROM e_cross.theta
+	            AND e_in.min_occurrence IS NOT DISTINCT FROM e_cross.min_occurrence
+	        ) OR (
+	            e_in.method_type = 'rationale' AND e_in.method_type = e_cross.method_type
+	            AND e_in.attribution_method IS NOT DISTINCT FROM e_cross.attribution_method
+	            AND e_in.scale_scores IS NOT DISTINCT FROM e_cross.scale_scores
+	            AND e_in.cumulative_scoring IS NOT DISTINCT FROM e_cross.cumulative_scoring
+	            AND e_in.threshold IS NOT DISTINCT FROM e_cross.threshold
+	        ) OR (
+	            e_in.method_type = 'span_pred' AND e_in.method_type = e_cross.method_type
+	        )
+	    )
 	)
 	SELECT * FROM cross_domain_max;
-
