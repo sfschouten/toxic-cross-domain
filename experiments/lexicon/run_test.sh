b6473413366b59c637ee4687480cf1a95d60f0eb
Stefan F. Schouten <sfschouten@gmail.com>
2023-03-01 20:29:30 +0100

    fix run_all.sh

---
+++ experiments/lexicon/run_test.sh
@@ -0,0 +1,1 @@
+python3 ../../src/toxic_x_dom/lexicon/test.py $*
--- run_all.sh
+++ run_all.sh
@@ -2,28 +2,28 @@
 
 pushd experiments/
   pushd rationale_extraction/
-#    ./run.sh bert_cad.json
-#    ./run.sh bert_semeval.json
-#    ./run.sh bert_hatexplain.json
-#    ./run_test.sh bert_cad.json
+    ./run.sh bert_cad.json
+    ./run.sh bert_semeval.json
+    ./run.sh bert_hatexplain.json
+    ./run_test.sh bert_cad.json
     ./run_test.sh bert_semeval.json
     ./run_test.sh bert_hatexplain.json
   popd
   pushd span_detection/grid/
-#    ./run.sh bert_cad.json
-#    ./run.sh bert_semeval.json
-#    ./run.sh bert_hatexplain.json
-#    ./run.sh bert_crf_cad.json
-#    ./run.sh bert_crf_semeval.json
-#    ./run.sh bert_crf_hatexplain.json
-#    ./run_test.sh bert_cad.json
+    ./run.sh bert_cad.json
+    ./run.sh bert_semeval.json
+    ./run.sh bert_hatexplain.json
+    ./run.sh bert_crf_cad.json
+    ./run.sh bert_crf_semeval.json
+    ./run.sh bert_crf_hatexplain.json
+    ./run_test.sh bert_cad.json
     ./run_test.sh bert_semeval.json
-#    ./run_test.sh bert_hatexplain.json
-#    ./run_test.sh bert_crf_cad.json
+    ./run_test.sh bert_hatexplain.json
+    ./run_test.sh bert_crf_cad.json
     ./run_test.sh bert_crf_semeval.json
     ./run_test.sh bert_crf_hatexplain.json
   popd
   pushd lexicon/
-#    ./run.sh
+    ./run.sh
   popd
 popd
